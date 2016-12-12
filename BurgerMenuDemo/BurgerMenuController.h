0b2007dfb9181a925927db6d0e66d16675111f68
Erica Winberry <elaewin@gmail.com>
2016-12-12 10:23:26 -0800

    started burger menu demo.

---
+++ BurgerMenuDemo/BurgerMenuController.h
@@ -0,0 +1,13 @@
+//
+//  BurgerMenuController.h
+//  BurgerMenuDemo
+//
+//  Created by Erica Winberry on 12/12/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BurgerMenuController : UIViewController
+
+@end
